7128a2de94ce551878a5a7be0c74bb10b4741693
xiawh <503962896@qq.com>
2021-02-07 16:17:09 +0800

    熟悉inappwebview

---
+++ lib/main_19.dart
@@ -0,0 +1,92 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: InAppWebViewPage());
+  }
+}
+
+class InAppWebViewPage extends StatefulWidget {
+  @override
+  _InAppWebViewPageState createState() => new _InAppWebViewPageState();
+}
+
+class _InAppWebViewPageState extends State<InAppWebViewPage> {
+  InAppWebViewController _webViewController;
+  ContextMenu contextMenu;
+  @override
+  void initState() {
+    super.initState();
+    contextMenu = ContextMenu(
+        menuItems: [
+          ContextMenuItem(
+              androidId: 1,
+              iosId: "1",
+              title: "Special",
+              action: () async {
+                print("Menu item Special clicked!");
+                var selectedText = await _webViewController.getSelectedText();
+                await _webViewController.clearFocus();
+                await _webViewController.evaluateJavascript(
+                    source: "window.alert('You have selected: $selectedText')");
+              })
+        ],
+        options: ContextMenuOptions(hideDefaultSystemContextMenuItems: false),
+        onCreateContextMenu: (hitTestResult) async {
+          print("onCreateContextMenu");
+          print(hitTestResult.extra);
+          print(await _webViewController.getSelectedText());
+        },
+        onHideContextMenu: () {
+          print("onHideContextMenu");
+        },
+        onContextMenuActionItemClicked: (contextMenuItemClicked) async {
+          var id = (Platform.isAndroid)
+              ? contextMenuItemClicked.androidId
+              : contextMenuItemClicked.iosId;
+          print("onContextMenuActionItemClicked: " +
+              id.toString() +
+              " " +
+              contextMenuItemClicked.title);
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text("InAppWebView")),
+        body: Container(
+            child: Column(children: <Widget>[
+          Expanded(
+            child: Container(
+              child: InAppWebView(
+                initialUrl: "https://github.com/flutter",
+                contextMenu: contextMenu,
+                initialOptions: InAppWebViewGroupOptions(
+                  crossPlatform: InAppWebViewOptions(
+                    debuggingEnabled: true,
+                  ),
+                ),
+                onWebViewCreated: (InAppWebViewController controller) {
+                  _webViewController = controller;
+                },
+              ),
+            ),
+          ),
+        ])));
+  }
+}
