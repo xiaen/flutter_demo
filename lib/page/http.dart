ca04ad56837262328f2b62ec1e2b3d8c761ff36a
xiawh <503962896@qq.com>
2021-03-08 18:14:12 +0800

    测试http请求

--- lib/page/http.dart
+++ lib/page/http.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HttpPage extends StatefulWidget {
   HttpPage({Key key}) : super(key: key);
@@ -18,19 +19,32 @@ import hcsr04
 hcsr04.open(3, 2)
 asomebot.ready(5, 6, 7, 8)
 asomebot.home()
-asomebot.forward()''';
+for count in range(20):
+  asomebot.forward(20, 40, 20, 0, 400, 400)
+asomebot.ready(5, 6, 7, 8)''';
+// a1=20, a2=40, a3=20, m=0, s1=150, s2=300
     var url = 'http://192.168.2.219:8003';
     print('------------------start');
-    Dio dio = Dio(BaseOptions(
-      contentType: Headers.formUrlEncodedContentType,
-    ));
-
+    Dio dio = Dio();
     await dio.post(url, data: code);
 
     // var httpClient = new HttpClient();
     // var request = await httpClient.postUrl(Uri.parse(url));
+    // request.headers.set('Content-1-Length', 900);
     // request.add(utf8.encode(code));
     // await request.close();
+
+    // var headers = Map<String, String>();
+    // headers["Content-Length"] = '109';
+
+    // var jsonParams = utf8.encode(code);
+
+    // var httpClient = http.Client();
+
+    // var uri = Uri.parse(url);
+
+    // http.Response response =
+    //     await httpClient.post(uri, body: jsonParams, headers: headers);
     print('------------------end');
   }
 
